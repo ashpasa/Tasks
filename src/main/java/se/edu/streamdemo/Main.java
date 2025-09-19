2d44b54333875e360bc566a8465ee9b87dfe89a7
ashpasa <asherpang03@gmail.com>
2025-09-19 17:07:25 +0800

    Add code to count deadlines using streams

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -19,7 +19,15 @@ public class Main {
         printDeadlines(tasksData);
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+        System.out.println("Total number of deadlines (using streams): " + countDeadlinesUsingStreams(tasksData));
 
+    }
+
+    private static int countDeadlinesUsingStreams(ArrayList<Task> tasksData) {
+        int count = (int) tasksData.stream()
+                .filter((Task t) -> t instanceof Deadline)
+                .count();
+        return count;
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
